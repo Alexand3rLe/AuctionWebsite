dd95454ddfd5e4f7f519317c6321c5c3edb1dcbc
ktcv31 <tatiana.carvajal0831@gmail.com>
2024-07-25 17:31:00 -0500

    handle bars front end with for each loop

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,28 @@
 const router = require('express').Router();
 const api = require('./api');
+const Task = require('../models/Task');
 
+router.use("/api", api);
 
-router.use("/api",api);
-
-router.get("/auction", (req, res) => {
-    res.json('hello world!');
+router.get('/', async (req, res) => {
+  const allTasks = await Task.findAll();
+  const sanitizedData = allTasks.map((task) => {
+    return task.get({ plain: true});
+  })
+  console.log(allTasks);
+  console.log('=========================')
+  console.log(sanitizedData);
+  // const testData = {
+  //   name: 'test work',
+  //   description: 'work to do',
+  //   status: 'in progress'
+  // };
+  res.render('todo', { sanitizedData });
 })
 
-module.exports = router; 
+
+router.get("/auction", (req, res) => {
+  res.json('hello!');
+})
+
+module.exports = router;
