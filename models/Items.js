c9e325286b64c55c88e4fdf71549fd96941d0a70
ktcv31 <tatiana.carvajal0831@gmail.com>
2024-07-29 17:44:25 -0500

    updated items table

--- models/Items.js
+++ models/Items.js
@@ -1,38 +1,40 @@
-const {Model, Datatypes, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-
-class Items extends Model {};
+class Items extends Model {}
 
 Items.init({
-    id: {
-       type: DataTypes.INTEGER,
-       allowNull: false,
-       primaryKey: true,
-       autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    price: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    status: {
-        type: DataTypes.STRING, 
-        allowNull: false
-    },
-    //picture: {}
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING, 
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING, // URL of the image
+    allowNull: true
+  }
 }, {
-    sequelize, 
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'items'
+  sequelize, 
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'items'
 });
 
 module.exports = Items;
